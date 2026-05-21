895e7c6aa2d5938ac9b0e901ab04213fed2931ef
lilmsa284 <shahzaibabid4@gmail.com>
2026-05-21 10:08:22 +0500

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Jenkins CI Pipeline!");
+
+        // Simple build step simulation
+        System.out.println("Building project... DONE");
+
+        // Simple test simulation
+        int result = add(2, 3);
+
+        if (result == 5) {
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("TEST FAILED");
+        }
+
+        System.out.println("Pipeline Finished Successfully!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
